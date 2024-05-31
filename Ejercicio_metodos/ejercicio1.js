2018567a6cd1cf2a33f3cfbddcaf7f21ab8bf72c
Alison Gonzalez Herrera <alisonherrera442@>
2024-05-30 19:47:19 -0500

    ejercicios

---
+++ Ejercicio_metodos/ejercicio1.js
@@ -0,0 +1,9 @@
+let persona = {
+    nombre: "Alison",
+    edad: 19,
+};
+
+
+console.log(persona.nombre);
+console.log(persona.edad);
+console.log(`hola mi nombre es ${persona.nombre}, tengo ${persona.edad} años`);
